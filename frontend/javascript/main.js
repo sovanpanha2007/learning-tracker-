036c603b7a266d799c603b5cd3e6ecc648e272b1
sovanpanha2007 <sovanpanha2007@gmail.com>
2025-10-25 00:42:22 +0700

    initial commit

---
+++ frontend/javascript/main.js
@@ -0,0 +1,13 @@
+
+
+console.log("Hello, world!");
+function add(a,b) { return a + b; }
+
+let a =10, b=20;
+console.log( add(a, b) );
+
+console.log(`My first number is ${a} and my added number is ${add(a, b)}`);
+
+let bool = 7>1;
+console.log(`Is 7 greater than 1? ${bool}`);
+console.log(typeof 1);
