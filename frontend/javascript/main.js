ff18a505109390505947ebe3c1db990453a2fe49
sovanpanha2007 <sovanpanha2007@gmail.com>
2025-10-30 23:46:04 +0700

    30.10.25, 11:45

--- frontend/javascript/main.js
+++ frontend/javascript/main.js
@@ -1,5 +1,3 @@
-
-
 // console.log("Hello, world!");
 // function add(a,b) { return a + b; }
 
@@ -7,18 +5,15 @@
 
 // console.log(`My first number is ${a} and my added number is ${add(a, b)}`);
 
-
 // let bool = 7>1;
 // console.log(`Is 7 greater than 1? ${bool}`);
 // console.log(typeof 1);
 
-
 // // console.log(a); // ❌
 // // console.log(b); // ❌
-// console.log(c); 
+// console.log(c);
 
 // var c = 3;
-
 
 // let pen = {
 //     price : 5,
@@ -30,20 +25,27 @@
 // }
 // console.log(pencil.price);
 
-console.log('number'+1);
-let a =123;
-console.log(a.toString() ==='123');
+console.log("number" + 1);
+let a = 123;
+console.log(a.toString() === "123");
 
 const arr = [];
-arr.push('1');
-arr.push('2');
+arr.push("1");
+arr.push("2");
 arr.unshift(0);
 arr.shift();
-console.log(arr)
+console.log(arr);
 
 const coll = new Map();
-coll.set('name', 'John');
-coll.set('age', 30);
-console.log(coll.get('name'));
-console.log(coll.has('age'));
+coll.set("name", "John");
+coll.set("age", 30);
+console.log(coll.get("name"));
+console.log(coll.has("age"));
 console.log(coll.size);
+
+console.log("hello");
+
+let names = ["john", "josh", "joe", "josh"];
+
+console.log(names.indexOf("josh"));
+console.log("test git");
